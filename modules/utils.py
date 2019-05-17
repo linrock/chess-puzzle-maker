3cfe2b5f1eea62d5f5c02e2f88af2042eb764aa3
Linmiao Xu <linmiao.xu@gmail.com>
2019-05-18 02:36:26 +0900

    Distinguish between material diff and total

--- modules/utils.py
+++ modules/utils.py
@@ -8,8 +8,11 @@ def sign(a):
     else:
         return 0
 
-def material_difference(board):
+def material_total(board):
     return sum(v * (len(board.pieces(pt, True)) + len(board.pieces(pt, False))) for v, pt in zip([0,3,3,5.5,9], chess.PIECE_TYPES))
+
+def material_difference(board):
+    return sum(v * (len(board.pieces(pt, True)) - len(board.pieces(pt, False))) for v, pt in zip([0,3,3,5.5,9], chess.PIECE_TYPES))
 
 def material_count(board):
     return chess.pop_count(board.occupied)
@@ -20,12 +23,12 @@ def should_investigate(a, b, board):
     if a.cp is not None and b.cp is not None:
         if (((a.cp > -110 and a.cp < 850 and b.cp > 200 and b.cp < 850) 
             or (a.cp > -850 and a.cp < 110 and b.cp < -200 and b.cp > -850))
-            and material_difference(board) > 3
+            and material_total(board) > 3
             and material_count(board) > 6):
             return True
     elif (a.cp is not None
         and b.mate is not None
-        and material_difference(board) > 3):
+        and material_total(board) > 3):
         if ((a.cp < 110 and sign(b.mate) == -1) or (a.cp > -110 and sign(b.mate) == 1) ):
             return True
     elif a.mate and b.mate:
