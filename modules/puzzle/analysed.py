9425d399b6644140a8a301ddf205d1d21778d014
Linmiao Xu <linmiao.xu@gmail.com>
2019-05-18 02:05:23 +0900

    Remove unused analysis methods

--- modules/puzzle/analysed.py
+++ modules/puzzle/analysed.py
@@ -2,17 +2,3 @@ class Analysis(object):
     def __init__(self, move, evaluation):
         self.move = move
         self.evaluation = evaluation
-
-    def sign(self, val):
-        if val <= 0:
-            return -1
-        else:
-            return 1
-
-    def sort_val(self):
-        if self.evaluation.cp is not None:
-            return self.evaluation.cp
-        elif self.evaluation.mate is not None:
-            return self.sign(self.evaluation.mate) * (abs(100 + self.evaluation.mate)) * 10000
-        else:
-            return 0
