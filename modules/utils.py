3795df3be9b8e5d1e0f846ba2ddf47f4292bd054
Linmiao Xu <linmiao.xu@gmail.com>
2019-05-18 19:51:41 +0900

    Make this a little more readable

--- modules/utils.py
+++ modules/utils.py
@@ -28,17 +28,15 @@ def fullmove_string(board):
 # determine if the difference between position A and B
 # is worth investigating for a puzzle.
 def should_investigate(a, b, board):
-    if a.cp is not None and b.cp is not None:
-        if (((a.cp > -110 and a.cp < 850 and b.cp > 200 and b.cp < 850) 
-            or (a.cp > -850 and a.cp < 110 and b.cp < -200 and b.cp > -850))
-            and material_total(board) > 3
-            and material_count(board) > 6):
-            return True
-    elif (a.cp is not None
-        and b.mate is not None
-        and material_total(board) > 3):
-        if ((a.cp < 110 and sign(b.mate) == -1) or (a.cp > -110 and sign(b.mate) == 1) ):
-            return True
+    if a.cp is not None and material_total(board) > 3:
+        if b.cp is not None and material_count(board) > 6:
+            if a.cp > -110 and a.cp < 850 and b.cp > 200 and b.cp < 850:
+                return True
+            elif a.cp > -850 and a.cp < 110 and b.cp < -200 and b.cp > -850:
+                return True
+        elif b.mate:
+            if (a.cp < 110 and sign(b.mate) == -1) or (a.cp > -110 and sign(b.mate) == 1):
+                return True
     elif a.mate and b.mate:
         if sign(a.mate) == sign(b.mate): #actually means that they're opposite
             return True
