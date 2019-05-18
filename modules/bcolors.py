2952612cae3c91290e343acf7d6fc76148a9338b
Linmiao Xu <linmiao.xu@gmail.com>
2019-05-19 08:47:58 +0900

    Switch back to ansi codes since colorama bright is actually bold

--- modules/bcolors.py
+++ modules/bcolors.py
@@ -1,13 +1,13 @@
 from colorama import Fore, Style
 
 class bcolors:
-    HEADER = Style.BRIGHT + Fore.MAGENTA
-    BLUE = Style.BRIGHT + Fore.BLUE
-    GREEN = Style.BRIGHT + Fore.GREEN
-    GRAY = Style.DIM
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    DIM = Style.DIM
 
-    WARNING = Style.BRIGHT + Fore.YELLOW
-    FAIL = Style.BRIGHT + Fore.RED
-    ENDC = Style.RESET_ALL
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
