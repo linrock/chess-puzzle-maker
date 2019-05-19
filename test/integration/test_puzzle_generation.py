52651c9201ef5f9909ba78b47734825b8eeac48d
Linmiao Xu <linmiao.xu@gmail.com>
2019-05-19 15:35:47 +0900

    Add integration test for generating a mate-in-3 puzzle

--- test/integration/test_puzzle_generation.py
+++ test/integration/test_puzzle_generation.py
@@ -18,6 +18,20 @@ class TestPuzzleIsComplete(unittest.TestCase):
         puzzle.generate()
         self.assertFalse(puzzle.is_complete())
 
+    def test_mate_in_3_is_complete(self):
+        # 1. Rxh7+ Kxh7 2. Rh1+ Kg7 3. Qh6#
+        board = chess.Board(
+            '3q1r1k/2p4p/1p1pBrp1/p2Pp3/2PnP3/5PP1/PP1Q2K1/5R1R w - - 1 0'
+        )
+        puzzle = Puzzle(
+            board,
+            board.parse_san('Rxh7+'),
+            chess.pgn.Game(),
+            False,
+        )
+        puzzle.generate()
+        self.assertTrue(puzzle.is_complete())
+
 
 if __name__ == '__main__':
     unittest.main()
