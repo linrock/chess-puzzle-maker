8aee54f173a34df8df0b019dfc4d9a759520f1ab
Linmiao Xu <linmiao.xu@gmail.com>
2019-05-20 09:37:13 +0900

    Use search depth 10 in puzzle generation tests

--- test/integration/test_puzzle_generation.py
+++ test/integration/test_puzzle_generation.py
@@ -5,6 +5,8 @@ import chess.pgn
 
 from modules.puzzle import Puzzle
 
+SEARCH_DEPTH = 10
+
 class TestPuzzleIsComplete(unittest.TestCase):
 
     def test_puzzle_is_not_complete(self):
@@ -14,7 +16,7 @@ class TestPuzzleIsComplete(unittest.TestCase):
             chess.Move.from_uci("e2e4"),
             chess.pgn.Game(),
         )
-        puzzle.generate()
+        puzzle.generate(depth=SEARCH_DEPTH)
         self.assertFalse(puzzle.is_complete())
 
     def test_mate_in_3_is_complete_1(self):
@@ -23,7 +25,7 @@ class TestPuzzleIsComplete(unittest.TestCase):
             'r1b3kr/ppp1Bp1p/1b6/n2P4/2p3q1/2Q2N2/P4PPP/RN2R1K1 w - - 1 0'
         )
         puzzle = Puzzle(board, board.parse_san('Qxh8+'), chess.pgn.Game())
-        puzzle.generate()
+        puzzle.generate(depth=SEARCH_DEPTH)
         self.assertTrue(puzzle.is_complete())
         self.assertTrue(len(puzzle.position_list_node.move_list()) == 4)
 
@@ -33,7 +35,7 @@ class TestPuzzleIsComplete(unittest.TestCase):
             'r2n1rk1/1ppb2pp/1p1p4/3Ppq1n/2B3P1/2P4P/PP1N1P1K/R2Q1RN1 b - - 0 1'
         )
         puzzle = Puzzle(board, board.parse_san('Qxf2+'), chess.pgn.Game())
-        puzzle.generate()
+        puzzle.generate(depth=SEARCH_DEPTH)
         self.assertTrue(puzzle.is_complete())
         self.assertTrue(len(puzzle.position_list_node.move_list()) == 4)
 
@@ -43,12 +45,22 @@ class TestPuzzleIsComplete(unittest.TestCase):
             '3q1r1k/2p4p/1p1pBrp1/p2Pp3/2PnP3/5PP1/PP1Q2K1/5R1R w - - 1 0'
         )
         puzzle = Puzzle(board, board.parse_san('Rxh7+'), chess.pgn.Game())
-        puzzle.generate()
+        puzzle.generate(depth=SEARCH_DEPTH)
         self.assertTrue(puzzle.is_complete())
         self.assertEqual(
             puzzle.position_list_node.move_list(),
             ['h8h7', 'f1h1', 'h7g7', 'd2h6'],
         )
+
+    def test_threefold_repetition_detection(self):
+        # https://lichess.org/tYLGlqsX
+        # 21... Be6??
+        board = chess.Board(
+            'r1b2r1k/ppp2p1p/8/P3p2p/2PqP3/3P1Q1P/6PK/5R2 b - - 3 21'
+        )
+        puzzle = Puzzle(board, board.parse_san('Be6'), chess.pgn.Game())
+        puzzle.generate(depth=SEARCH_DEPTH)
+        self.assertTrue(puzzle.is_complete())
 
 
 if __name__ == '__main__':
