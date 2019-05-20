39f5e6f58f71f1b6a3e8387c3d67c68456816341
Linmiao Xu <linmiao.xu@gmail.com>
2019-05-20 18:08:26 +0900

    Invoke command for running all tests

--- tasks.py
+++ tasks.py
@@ -1,7 +1,10 @@
 from invoke import task
 
 @task
-def test(c, test_type):
+def test(c, test_type=None):
     """ Run all unit or integration tests
     """
-    c.run("python3 -m unittest test/%s/test_*.py" % test_type, pty=True)
+    if test_type:
+        c.run("python3 -m unittest test/%s/test_*.py" % test_type, pty=True)
+    else:
+        c.run("python3 -m unittest test/*/test_*.py", pty=True)
