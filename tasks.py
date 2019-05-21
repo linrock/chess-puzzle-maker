c8ab4ce5c4605f4f2736012c46b736fc02655038
Linmiao Xu <linmiao.xu@gmail.com>
2019-05-21 19:26:48 +0900

    Add a task to downlaod lichess tournament games

--- tasks.py
+++ tasks.py
@@ -1,10 +1,21 @@
 from invoke import task
 
 @task
-def test(c, test_type=None):
+def test(c, unit=False, integration=False):
     """ Run all unit or integration tests
     """
-    if test_type:
-        c.run("python3 -m unittest test/%s/test_*.py" % test_type, pty=True)
+    if unit:
+        cmd = "python3 -m unittest test/unit/test_*.py"
+    elif integration:
+        cmd = "python3 -m unittest test/integration/test_*.py"
     else:
-        c.run("python3 -m unittest test/*/test_*.py", pty=True)
+        cmd = "python3 -m unittest test/*/test_*.py"
+    c.run(cmd, pty=True)
+
+@task
+def fetch_lichess(c, tournament=''):
+    """ Fetch all games played in a lichess tournament
+    """
+    if len(tournament):
+        id = tournament
+        c.run("curl https://lichess.org/api/tournament/%s/games > tournament.%s.pgn" % (id, id), pty=True)
