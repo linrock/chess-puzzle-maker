b7de9963a8e1fee96577ad3211ecf68895cdd7fb
Linmiao Xu <linmiao.xu@gmail.com>
2019-05-22 08:57:54 +0900

    Enable creating puzzles via command line from a FEN string

--- main.py
+++ main.py
@@ -5,12 +5,13 @@
 
 import argparse
 import logging
-import os
 import sys
 
+from chess import Board
 import chess.pgn
 
 from modules.bcolors import bcolors
+from modules.puzzle import Puzzle
 from modules.puzzle_finder import find_puzzle_candidates
 from modules.analysis import engine
 
@@ -19,11 +20,12 @@ parser = argparse.ArgumentParser(
     description=__doc__,
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
-parser.add_argument("games", metavar="GAMES", nargs="?", type=str, default="games.pgn",
-                    help="A PGN file with games to scan for puzzles")
 
-parser.add_argument("--output", metavar="OUTPUT_PGN", default="tactics.out.pgn",
-                    help="An output pgn file")
+group = parser.add_argument_group('data inputs')
+group.add_argument("--games", metavar="GAMES", nargs="?", type=str, default=None,
+                    help="A PGN file with games to scan for puzzles")
+group.add_argument("--fen", metavar="FEN", nargs="?", type=str, default=None,
+                    help="A FEN position to generate a puzzle from")
 
 # Chess engine settings
 group = parser.add_argument_group('chess engine settings')
@@ -36,6 +38,8 @@ group.add_argument("--scan-depth", metavar="DEPTH", nargs="?", type=int, default
 group.add_argument("--search-depth", metavar="DEPTH", nargs="?", type=int, default=22,
                     help="depth for searching a position for candidate moves")
 
+parser.add_argument("--output", metavar="OUTPUT_PGN", default="generated_puzzles.pgn",
+                    help="An output PGN file")
 parser.add_argument("--quiet", dest="loglevel",
                     default=logging.DEBUG, action="store_const", const=logging.INFO,
                     help="substantially reduce the number of logged messages")
@@ -55,17 +59,39 @@ except ImportError:
 logging.basicConfig(format="%(message)s", level=settings.loglevel, stream=sys.stdout)
 logging.getLogger("chess").setLevel(logging.WARNING)
 
-all_games = open(settings.games, "r")
-
 engine.configure({
   'Threads': settings.threads,
   'Hash': settings.memory,
   'Contempt': 0,
 })
 
+def write_puzzle_to_file(puzzle, output_file, pgn_headers=None):
+    puzzle_pgn = puzzle.to_pgn(pgn_headers=pgn_headers)
+    logging.debug(bcolors.MAGENTA + "NEW PUZZLE GENERATED\n" + bcolors.ENDC)
+    logging.info(bcolors.CYAN + puzzle_pgn + bcolors.ENDC)
+    with open(output_file, "a") as f:
+        f.write(puzzle_pgn)
+        f.write("\n\n")
+
+
+# load a FEN and try to create a puzzle from it
+
+if settings.fen:
+    puzzle = Puzzle(Board(settings.fen))
+    puzzle.generate(depth=settings.search_depth)
+    if puzzle.is_complete():
+        write_puzzle_to_file(puzzle, settings.output)
+    engine.quit()
+    exit(0)
+
+
+# load games from a PGN and scan them for puzzles
+
 n_positions = 0   # number of positions considered
 n_puzzles = 0     # number of puzzles generated
 game_id = 0
+
+all_games = open(settings.games, "r")
 
 while True:
     game = chess.pgn.read_game(all_games)
@@ -87,16 +113,9 @@ while True:
             bcolors.ENDC
         )
         puzzle.generate(settings.search_depth)
-        if not puzzle.is_complete():
-            continue
-        puzzle_pgn = puzzle.to_pgn(pgn_headers=game.headers)
-        logging.debug(bcolors.MAGENTA + "NEW PUZZLE GENERATED\n" + bcolors.ENDC)
-        logging.info(bcolors.CYAN + puzzle_pgn + bcolors.ENDC)
-        tactics_file = open(settings.output, "a")
-        tactics_file.write(puzzle_pgn)
-        tactics_file.write("\n\n")
-        tactics_file.close()
-        n_puzzles += 1
+        if puzzle.is_complete():
+            write_puzzle_to_file(puzzle, settings.output, pgn_headers=game.headers)
+            n_puzzles += 1
 
 logging.debug(
     bcolors.MAGENTA +
