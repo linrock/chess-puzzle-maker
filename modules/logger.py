f8e3e27c49e7dd8bf6c72dd70e42514014108f6f
Linmiao Xu <linmiao.xu@gmail.com>
2019-05-24 04:23:00 +0900

    Enable using unicode pieces for printed board position

--- modules/logger.py
+++ modules/logger.py
@@ -16,7 +16,7 @@ def configure_logging(level=logging.DEBUG):
 def log(color: str, message: str):
     logging.debug(color + message + Color.ENDC)
 
-def log_board(board: Board):
+def log_board(board: Board, unicode_pieces=False):
     """ Logs the fen string and board representation
     """
     color = Color.BLACK
@@ -24,6 +24,19 @@ def log_board(board: Board):
     board_str = "\n  " + str(board).replace("\n", "\n  ")
     board_str = re.sub("[a-z]", lambda p: Color.DARK_GREY + p[0] + color, board_str)
     board_str = re.sub("[A-Z]", lambda p: Color.WHITE + p[0] + color, board_str)
+    if unicode_pieces:
+        board_str = board_str.replace("k", Color.DIM + "\u2654")
+        board_str = board_str.replace("K", Color.WHITE + "\u2654")
+        board_str = board_str.replace("q", Color.DIM + "\u2655")
+        board_str = board_str.replace("Q", Color.WHITE + "\u2655")
+        board_str = board_str.replace("r", Color.DIM + "\u2656")
+        board_str = board_str.replace("R", Color.WHITE + "\u2656")
+        board_str = board_str.replace("b", Color.DIM + "\u2657")
+        board_str = board_str.replace("B", Color.WHITE + "\u2657")
+        board_str = board_str.replace("n", Color.DIM + "\u2658")
+        board_str = board_str.replace("N", Color.WHITE + "\u2658")
+        board_str = board_str.replace("p", Color.DIM + "\u2659")
+        board_str = board_str.replace("P", Color.WHITE + "\u2659")
     log(color, board_str + "\n")
 
 def log_move(board: Board, move: Move, score: Score,
